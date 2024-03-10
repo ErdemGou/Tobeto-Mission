e82fd2813a1eb6b3573e08a26c3e191fc7354d19
Erdem KÖSTEK <84468790+ErdemGou@users.noreply.github.com>
2024-03-10 15:07:36 +0300

    Yeni Görevler 2
    Yeni Görevler 2

---
+++ Tobeto Mission/Mission-8~54/CSharpCourse/Interfaces/Program.cs
@@ -0,0 +1,87 @@
+﻿using Interfaces;
+using Microsoft.VisualBasic;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //InterfacesIntro();
+        //Demo();
+
+        ICustomerDal[] customerDals = new ICustomerDal[3]
+        {
+            new SqlServerCustomerDal(),
+            new OracleServerCustomerDal(),
+            new MySqlServerCustomerDal()
+        };
+
+        foreach (var customerDal in customerDals)
+        {
+            customerDal.Add();
+        }
+
+        Console.ReadLine();
+    }
+
+    private static void Demo()
+    {
+        CustomerManager customerManager = new CustomerManager();
+        customerManager.Add(new OracleServerCustomerDal());
+    }
+
+    private static void InterfacesIntro()
+    {
+        PersonManager manager = new PersonManager();
+        Customer customer = new Customer() { Id = 1, FirstName = "Engin", LastName = "Demiroğ", Address = "Ankara" };
+
+        Student student = new Student()
+        {
+            Id = 1,
+            FirstName = "Derin",
+            LastName = "Demiroğ",
+            Departmant = "Computer Sciences"
+        };
+        manager.Add(customer);
+        manager.Add(student);
+    }
+}
+
+interface IPerson
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    
+}
+
+class Customer : IPerson
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Address { get; set; }
+}
+
+class Student:IPerson
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Departmant {  get; set; }
+}
+
+class Worker : IPerson
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Departmant { get; set; }
+}
+
+class PersonManager
+{
+    public void Add(IPerson person)
+    {
+        Console.WriteLine(person.FirstName);
+    }
+}
